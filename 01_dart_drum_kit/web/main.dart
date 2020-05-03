ed7bba52de7982e6cbcb57c6b9b8b9fc3d16c327
ALMaclaine <almaclaine@gmail.com>
2020-05-03 01:00:39 -0700

    finish 01_dart_drum_kit

---
+++ 01_dart_drum_kit/web/main.dart
@@ -0,0 +1,23 @@
+import 'dart:html';
+
+import 'dart:js';
+
+void removeTransition(Event e) {
+  if((e as TransitionEvent).propertyName != 'transform') return;
+  e.target as HtmlElement ..classes.remove('playing');
+}
+
+void playSound(Event e) {
+  final key = e as KeyboardEvent;
+  final audio = querySelector('audio[data-key="${key.keyCode}"]') as AudioElement;
+  final keyE = querySelector('div[data-key="${key.keyCode}"]') as HtmlElement;
+  if(audio == null) return;
+  keyE.classes.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+}
+
+void main() {
+  querySelectorAll('.key').forEach((e) => e.addEventListener('transitionend', removeTransition));
+  context['window'] as Window ..addEventListener('keydown', playSound);
+}
