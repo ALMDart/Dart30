ae2d8efb566ce6b2b5417e616cee83f37fa3c19b
ALMaclaine <almaclaine@gmail.com>
2020-05-04 13:05:46 -0700

    add 05_flex_panel_gallery

---
+++ 05_dart_flex_panel_gallery/web/main.dart
@@ -0,0 +1,21 @@
+import 'dart:html';
+
+
+void toggleOpen(Event e) {
+  print('Hello');
+  e.currentTarget as HtmlElement..classes.toggle('open');
+}
+
+void toggleActive(Event e) {
+  final tEvent = e as TransitionEvent;
+  print(tEvent.propertyName);
+  if (tEvent.propertyName.contains('flex')) {
+    e.currentTarget as HtmlElement..classes.toggle('open-active');
+  }
+}
+
+void main() {
+  final panels = querySelectorAll('.panel');
+  panels.forEach((panel) => panel.addEventListener('click', toggleOpen));
+  panels.forEach((panel) => panel.addEventListener('transitionend', toggleActive));
+}
