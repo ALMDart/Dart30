2e8c5cdb0421d158973d5187547d804ad3d241e4
ALMaclaine <almaclaine@gmail.com>
2020-05-19 16:18:25 -0700

    finish 28_dart_video_speed_controller

---
+++ 28_dart_video_speed_controller/web/main.dart
@@ -0,0 +1,25 @@
+import 'dart:html';
+
+final speed = document.querySelector('.speed');
+final bar = speed.querySelector('.speed-bar');
+final video = document.querySelector('.flex') as VideoElement;
+
+void handleMove(Event e) {
+  final mouseEvent = e as MouseEvent;
+  final target = e.currentTarget as HtmlElement;
+  final y = mouseEvent.page.y - target.offsetTop;
+  final percent = y / target.offsetHeight;
+  final min = 0.4;
+  final max = 4;
+  final height = '${(percent * 100).round()}%';
+  final playbackRate = percent * (max - min) + min;
+  bar.style.height = height;
+  bar.text = playbackRate.toStringAsFixed(2) + '×';
+  video.playbackRate = playbackRate;
+}
+
+
+
+void main() {
+  speed.addEventListener('mousemove', handleMove);
+}
